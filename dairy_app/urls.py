e27914c183ce3725d405659415e21dccc71341f7
tonnymwanza <tonnimwanza@gmail.com>
2024-05-31 22:56:40 +0300

    the latest commits

--- dairy_app/urls.py
+++ dairy_app/urls.py
@@ -3,10 +3,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from . views import HomeView
+from . views import AboutView
+from . views import ServicesView
+from . views import ContactView
+from . views import ProductView
+from . import views
 # my urls
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home')
+    path('', HomeView.as_view(), name='home'),
+    path('about', AboutView.as_view(), name='about'),
+    path('services', ServicesView.as_view(), name='services'),
+    path('contact', ContactView.as_view(), name='contact'),
+    path('products', ProductView.as_view(), name='products'),
+    path('user_register', views.user_register, name='user_register'),
+    path('user_login', views.user_login, name='user_login'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- dairy_app/views.py
+++ dairy_app/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth import authenticate
+from django.contrib import messages
 
 from django.views import View
 # Create your views here.
@@ -7,3 +11,62 @@ class HomeView(View):
 
     def get(self, request):
         return render(request, 'index.html')    
+
+class AboutView(View):
+
+    def get(self, request):
+        return render(request, 'about.html')
+    
+class ServicesView(View):
+
+    def get(self, request):
+        return render(request, 'services.html')
+    
+class ContactView(View):
+
+    def get(self, request):
+        return render(request, 'contact.html')
+    
+class ProductView(View):
+
+    def get(self, request):
+        return render(request, 'product.html')
+
+def user_register(request): # user registration
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'the username is in user. try another one')
+                return redirect('user_register')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'the email is in use. find another one')
+                return redirect('user_register')
+            else:
+                user = User.objects.create_user(username = username, email = email, password = password)
+                return redirect('user_login')
+        else:
+            messages.error(request, 'ensure the passwords are matching')
+            return redirect('user_register')
+    return render(request, 'register.html')
+
+def user_login(request): # user login
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            if 'next' in request.POST:
+                return redirect(request.POST['next'])
+            else:
+                return redirect('home')
+        else:
+            messages.error(request, 'invalid details provided. make sure the password or username is correct')
+            return redirect('user_login')
+    return render(request, 'login.html')
